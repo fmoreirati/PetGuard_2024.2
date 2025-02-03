666a09dccf81d5f54042641562a025a3db50cc72
Vitor Burnier <burnvitor2@gmail.com>
2025-02-03 15:53:44 -0300

    html spa

--- js/funcoes.js
+++ js/funcoes.js
@@ -28,38 +28,89 @@ function template(){
             </div>
 
             <div>
-                <a href="perfil.html"><img src="image/user.png" alt=""></a>
-                <span></span>
+                <!-- OFFCANVAS NO DESKTOP ()> 1020px
+                <button class="btn btn-success" type="button" data-bs-toggle="offcanvas"data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+                    <span><i class="fa-solid fa-cart-shopping"></i></span>carrinho
+               </button>
+               <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+                   id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+                   <div class="offcanvas-header">
+                       <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+                       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                   </div>
+                   <div class="offcanvas-body">
+                       <p>Try scrolling the rest of the page to see this option in action.</p>
+                   </div>
+               </div> -->
+
+                <div id="carrinhoHeader"></div>
+
+                <a href="" id="imageUser"><img src="image/user.png" alt=""></a>
             </div>
         </header>
 
-        <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
-                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a href="" title="inicio"><i class="fa-solid fa-house fa-fw"></i><span>home</span></a>
+                <div class="collapse navbar-collapse mb-2" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"><i class="fa-brands fa-whatsapp fa-2xl" style="color: #0d8c66;"></i></a>
+                        </li>
 
-                        <a href="" title="carrinho"><i class="fa-solid fa-cart-shopping"></i><span>carrinho</span></a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Cachorro</button>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                        </div>
 
-                        <a href="" title="contato"><i class="fa-solid fa-comments fa-fw"></i><span>sac</span></a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Gato</button>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                        </div>
 
-                        <a href="" title="Sobre"><i class="fa-solid fa-circle-info fa-fw"></i><span>info</span></a>
+                        <div class="dropdown">
+                            <button class="dropbtn">Pássaro</button>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                        </div>
 
-                    </div>
-                </div>
-            </div>
-        </nav>
+                        <div class="dropdown">
+                            <button class="dropbtn">Peixe</button>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                        </div>
 
-        <main>
-        </main>
-
-        <footer>
-            &copy; <span>direitos reservados</span>
-        </footer>
-    </div>`
+                        <div class="dropdown">
+                            <button class="dropbtn">Outros</button>
+                            <div class="dropdown-content">
+                              <a href="#">Link 1</a>
+                              <a href="#">Link 2</a>
+                              <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                        
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>`
 }
