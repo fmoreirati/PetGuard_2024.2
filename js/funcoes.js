9e3bd92376631aa101f7cb5566c0e3d68be43224
Vitor Burnier <burnvitor2@gmail.com>
2025-02-03 15:56:54 -0300

    novo html no spa

--- js/funcoes.js
+++ js/funcoes.js
@@ -112,5 +112,14 @@ function template(){
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>`
+                    </form>
+        </nav>
+
+        <main></main>
+
+        
+        <footer>
+            &copy; <span>direitos reservados</span>
+        </footer>`
+
 }
