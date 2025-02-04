dbc4d666baa6dd453c62392f980eed7fd2a885a6
Vitor Burnier <burnvitor2@gmail.com>
2025-02-04 06:06:15 -0300

    font ajuste

--- js/funcoes.js
+++ js/funcoes.js
@@ -111,7 +111,7 @@ function template(){
                     </ul>
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        <button class="btn bg-success text-white rounded-5" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
                     </form>
         </nav>
 
